c2e0dde376e465f746e4e2dba5a5d4c596a47095
Cristian Perez <cperezlecaros@gmail.com>
2025-04-14 23:17:49 -0400

    Install config, typeorm, pg, and configure db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,30 @@ import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { AuthModule } from './auth/auth.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, CategoryModule, AuthModule, TransactionModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.getOrThrow('PG_URI'),
+        database: configService.getOrThrow('DB_NAME'),
+        ssl: true,
+        synchronize: configService.getOrThrow('NODE_ENV') === 'production',
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    CategoryModule,
+    AuthModule,
+    TransactionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
