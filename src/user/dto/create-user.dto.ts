8fdb389de7725602535b014175bdcd6e6866a44c
Cristian Perez <cperezlecaros@gmail.com>
2025-04-15 23:10:26 -0400

    Install class validator and transform, update user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,21 @@
+import { IsEmail, IsStrongPassword } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail({}, { message: 'El correo electrónico no es válido.' })
   email: string;
+
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    {
+      message:
+        'La contraseña debe contener al menos 8 caracteres, una mayúscula, una minúscula, un número y un símbolo.',
+    },
+  )
   password: string;
 }
